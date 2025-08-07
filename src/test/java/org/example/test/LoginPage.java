b6614c66bd3efa8f68dd3c9ccf55d9fe3f04a469
Dan <daniel.voda@ext.evozon.com>
2025-08-07 13:36:03 +0300

    add loginRegisteredUserWithInvalidEmailFormat test

--- src/test/java/org/example/test/LoginPage.java
+++ src/test/java/org/example/test/LoginPage.java
@@ -4,6 +4,9 @@ import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.regex.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LoginPage {
 
@@ -11,14 +14,14 @@ public class LoginPage {
     private static Navigation nav;
 
     @BeforeAll
-    public static void startSessions(){
-        Navigation navi = new Navigation(150, true); // slowMo , headless true
+    public static void startSessions() {
+        Navigation navi = new Navigation(150, false); // slowMo , headless true
         nav = navi;
         page = nav.getPage();
     }
 
     @AfterAll
-    public static void closeSession(){
+    public static void closeSession() {
         nav.navigateClose();
     }
 
@@ -35,12 +38,31 @@ public class LoginPage {
     }
 
     @Test
-    public void loginValidUserTest(){
+    public void loginValidUserTest() {
         User testUser = new User("Jon","Jon","Jon@email.com","user1234"); // Create a registered user
-
         nav.navigateToLoginPage();
         fillEmail(testUser.getEmail());
         fillPassword(testUser.getPassword());
         submitLogin();
+        assertThat(nav.getPage()).hasTitle("My Account");
+        nav.logOutUser();
+        assertThat(nav.getPage()).hasTitle("Madison Island");
+    }
+
+    @Test
+    public void loginInvalidUserTest() {
+        nav.navigateToLoginPage();
+        submitLogin();
+        assertThat(nav.getPage()).hasTitle("Customer Login");
+    }
+
+    @Test
+    public void loginRegisteredUserWithInvalidEmailFormat(){
+        User testUser = new User("Jon","Jon","Jon","user1234"); // email: Jon
+        nav.navigateToLoginPage();
+        fillEmail(testUser.getEmail());
+        fillPassword(testUser.getPassword());
+        submitLogin();
+        assertThat(nav.getPage()).hasTitle(Pattern.compile(".*login.*", Pattern.CASE_INSENSITIVE));
     }
 }
